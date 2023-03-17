fae0ba9e7d1b22ff464bce907171e294d8551150
andersonmarqueszero <113368673+andersonmarqueszero@users.noreply.github.com>
2023-03-16 22:02:48 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const draggable_list = document.getElementById('draggable-list');
+const check = document.getElementById('check');
+
+const bestSellingFranchises = [
+    "Mario",
+    "Pokemon",
+    "Call of Duty",
+    "Grand Theft Auto",
+    "FIFA",
+    "The Legend of Zelda",
+    "Sonic the Hedgehog",
+    "Resident Evil",
+    "Tetris",
+    "Minecraft"
+  ];
+
+// Store listitems
+ const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+// Insert list items into DOM
+function createList() {
+    [...bestSellingFranchises]
+        .forEach((game, index) => {
+            const listItem = document.createElement('li');
+
+            listItem.setAttribute('data-index', index);
+
+            listItem.innerHTML = `
+                <span class="number">${index +1}</span>
+                <div class="draggable" draggable="true">
+                    <p class="game-name">${game}</p>
+                    <i class="fas fa-grip-lines"></i>
+                </div>
+            `;
+
+            listItems.push(listItem);
+
+            draggable_list.appendChild(listItem);
+        });
+}
